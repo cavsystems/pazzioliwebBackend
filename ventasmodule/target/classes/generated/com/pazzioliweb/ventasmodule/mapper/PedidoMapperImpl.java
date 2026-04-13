23e128a06e5274aabbdf5b6c1497d6fb562855c6
luis David <luisdacade@gmail.com>
2026-04-13 14:59:24 -0500

    cambiosbackluis

--- ventasmodule/target/classes/generated/com/pazzioliweb/ventasmodule/mapper/PedidoMapperImpl.java
+++ ventasmodule/target/classes/generated/com/pazzioliweb/ventasmodule/mapper/PedidoMapperImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-13T07:38:39-0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+    date = "2026-04-13T14:49:59-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.18 (Microsoft)"
 )
 @Component
 public class PedidoMapperImpl implements PedidoMapper {
