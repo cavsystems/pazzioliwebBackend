5b99e392d1af59c7e55dfd4219dbfebae057ee67
luis David <luisdacade@gmail.com>
2026-06-01 10:14:59 -0500

    cambiosluis

--- ventasmodule/target/generated-sources/annotations/com/pazzioliweb/ventasmodule/mapper/VentaMapperImpl.java
+++ ventasmodule/target/generated-sources/annotations/com/pazzioliweb/ventasmodule/mapper/VentaMapperImpl.java
@@ -18,8 +18,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-01T08:00:26-0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+    date = "2026-06-01T09:44:08-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.18 (Microsoft)"
 )
 @Component
 public class VentaMapperImpl implements VentaMapper {
