0128302a8c5dcfd436d230fca14b25cd16f7f90e
luis David <luisdacade@gmail.com>
2026-07-10 11:32:38 -0500

    cambiosback

--- comprasmodule/target/generated-sources/annotations/com/pazzioliweb/comprasmodule/mapper/OrdenCompraMapperImpl.java
+++ comprasmodule/target/generated-sources/annotations/com/pazzioliweb/comprasmodule/mapper/OrdenCompraMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-04T16:02:57-0500",
+    date = "2026-07-10T09:55:38-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.18 (Microsoft)"
 )
 @Component
